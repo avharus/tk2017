d27bc27f629684673ebaf2ec996c5b67e4a06eb5
avharus <wouter.leeuw@kpnmail.nl>
2016-12-05 17:13:15 +0100

    Added HTML and JS files

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿$("document").ready(function() {
+  $(".btn").on('click', function() {
+    var textId = $(this).attr('data-textId');
+    var socialId = $(this).attr('data-socialId');
+    $('#'+textId).slideToggle(200);
+    $('#'+socialId).slideToggle(200);
+  });
+});
