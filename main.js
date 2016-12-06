27a68c1c7d240f514cfd5b36b00a46e019b5c28b
avharus <wouter.leeuw@kpnmail.nl>
2016-12-06 15:16:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
   $(".btn").on('click', function() {
     var textId = $(this).attr('data-textId');
     var socialId = $(this).attr('data-socialId');
+    if (this.value=="Read more") {
+      this.value="Read less";
+    } else {
+      this.value="Read more";
+    }
     $('#'+textId).slideToggle(200);
     $('#'+socialId).slideToggle(200);
   });
